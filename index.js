46a42ed776621ac9f26dcd3f039d4577a80cc554
PARAMESHWARAN <104810544+Vkparamesh@users.noreply.github.com>
2022-09-14 22:58:03 +0530

    first rout completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const { Router } = require('express');
 const express = require('express');
+const { books } = require("./data/books.json");
+const { users } = require("./data/users.json");
+
 
 const app = express();
 
@@ -12,8 +16,57 @@ app.get("/", (req, res) => {
     })
 })
 
+
+// Router:/users
+// method:get
+// description:Get all users
+// access:Public 
+// parameter : None  
+
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        sucess: true,
+        data: users,
+    });
+})
+
+// Router:/users/id
+// method:get
+// description:Get particular person 
+// access:Public 
+// parameter : id  
+
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            sucess: false,
+            message: "user not found"
+        })
+    }
+    return res.status(200).json({
+        sucess: true,
+        data: user
+    })
+})
+// Router:/users
+// method:post
+// description: create the new user   
+// access:Public 
+// parameter : none
+
+app.post("/users", (req, res) => {
+
+})
+
+
+
+
+
+
 app.get("*", (req, res) => {
-    res.status(404).send(
+    res.status(404).json(
         {
             message: "thes route doesn't exist"
         }
