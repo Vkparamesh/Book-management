cbf02b3ce6006cca43df15887fa592b06036b16d
PARAMESHWARAN <104810544+Vkparamesh@users.noreply.github.com>
2022-09-14 19:59:18 +0530

    server fully commplited

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 
 const port = 8081;
 
@@ -9,6 +10,14 @@ app.get("/", (req, res) => {
     res.status(200).send({
         message: "hi"
     })
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send(
+        {
+            message: "thes route doesn't exist"
+        }
+    );
 })
 
 app.listen(port, (req, res) => {
